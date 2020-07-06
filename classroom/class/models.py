0c2dbefe3175bfcc0786e4f8812080e9ab23664f
parthpandyappp <parthpandyappp@gmail.com>
2020-07-07 00:17:53 +0530

    Added relationship among table Pswd & classroom

--- classroom/class/models.py
+++ classroom/class/models.py
@@ -3,13 +3,14 @@ from __future__ import unicode_literals
 from django.db import models
 
 class Pswd(models.Model):
-    passcode = models.CharField(max_length=50)
+    passcode = models.CharField(max_length=50, null=True)
 
     def __str__(self):
         return self.passcode
 
 class classroom(models.Model):
-    classname = models.CharField(max_length=50)
+    passcod = models.ForeignKey(Pswd, on_delete=models.CASCADE, null=True)
+    classname = models.CharField(max_length=50, null=True)
 
     def __str__(self):
         return self.classname
