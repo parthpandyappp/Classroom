3c191fd79f6c6a8aea44b86ba4a691b35fce298a
parthpandyappp <parthpandyappp@gmail.com>
2020-08-06 14:57:57 +0530

    Deleting trash class

--- class/admin.py
+++
@@ -1,8 +0,0 @@
-# -*- coding: utf-8 -*-
-from __future__ import unicode_literals
-from .models import Pswd, classroom
-from django.contrib import admin
-
-admin.site.register(Pswd)
-admin.site.register(classroom)
-# Register your models here.
