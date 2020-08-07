dc9aa12825a87ade6eb4c2c496f7982baecff476
IgnisDa <diptesh.choudhuri@gmail.com>
2020-08-07 14:11:33 +0530

    Fix signals, UserProfiles are now created

--- classnote/models.py
+++ classnote/models.py
@@ -1,5 +1,5 @@
-from django.db import models
 from django.contrib.auth.models import User
+from django.db import models
 from django.db.models.signals import post_save
 from django.dispatch import receiver
 
@@ -10,15 +10,23 @@ class Pswd(models.Model):
     def __str__(self):
         return self.passcode
 
+
 class UserProfile(models.Model):
-    user = models.OneToOneField(User, on_delete=models.CASCADE, related_name='profile', null=True, blank=True)
-    
+    user = models.OneToOneField(
+        User, on_delete=models.CASCADE, related_name='profile',
+        null=True, blank=True
+    )
+
     def __str__(self):
         return self.user.username
 
+
 class classroom(models.Model):
     classname = models.CharField(max_length=50, null=True)
-    user = models.OneToOneField(User, on_delete=models.CASCADE, related_name='user', null=True, blank=True)
+    user = models.OneToOneField(
+        User, on_delete=models.CASCADE, related_name='user', null=True,
+        blank=True
+    )
     code = models.ForeignKey(Pswd, on_delete=models.CASCADE, null=True)
     user_profile = models.ManyToManyField(UserProfile)
 
@@ -26,10 +34,8 @@ class classroom(models.Model):
         return self.classname
 
 
-
-"""def create_profile(sender, **kwargs):
-    user = kwargs["instance"]
-    if kwargs["created"]:
-        user_profile = UserProfile(user=user)
-        user_profile.save()
-post_save.connect(create_profile, sender=User)"""
+@receiver(post_save, sender=User)
+def create_or_update_user_profile(sender, instance, created, **kwargs):
+    if created:
+        UserProfile.objects.create(user=instance)
+    instance.profile.save()
