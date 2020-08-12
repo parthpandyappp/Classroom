002158e16890ad3bb25bf324ec3e4fdb53c89028
IgnisDa <ignisda2001@gmail.com>
2020-08-12 23:09:28 +0530

    Style some of the pages

--- classnote/urls.py
+++ classnote/urls.py
@@ -6,7 +6,7 @@ urlpatterns = [
     path('', views.index, name='index'),
     path('create/', views.ClassroomCreateView.as_view(), name='create'),
     path('join/<int:pk>/', views.ClassroomJoinView.as_view(), name="join"),
-    path('processing/', views.processing, name="processing"),
-    path('check/', views.check, name="check"),
+    #path('processing/', views.processing, name="processing"),
+    #path('check/', views.check, name="check"),
     path('register/' ,views.register, name="register"),
 ]
