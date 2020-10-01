c0b2b479da8c4fffced72d83aae09f674ad098a0
Shivang-Agarwal11 <65328598+Shivang-Agarwal11@users.noreply.github.com>
2020-10-01 11:31:00 +0530

    Add urls.py
    This is the url file for accounts app.

---
+++ accounts/urls.py
@@ -0,0 +1,13 @@
+from django.urls import path
+from accounts import views
+
+
+urlpatterns = [
+     path('', views.index, name='index'),
+    path('create/', views.create, name='click'),
+    path('join/', views.join, name="join"),
+    path('processing/', views.processing, name="processing"),
+    path('profile/', views.profile),
+    path('update-profile/', views.UserUpdatation),
+    path('register/', views.register, name="register"),
+]
