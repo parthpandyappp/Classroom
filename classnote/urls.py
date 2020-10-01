7f33943f4d57d97e6dbf8370f70334520ce5c0cb
Shivang-Agarwal11 <65328598+Shivang-Agarwal11@users.noreply.github.com>
2020-10-01 11:19:30 +0530

    Update urls.py

--- classnote/urls.py
+++ classnote/urls.py
@@ -1,13 +1,7 @@
-from django.urls import path
+from django.urls import path,include
 from . import views
 
 
 urlpatterns = [
-    path('', views.index, name='index'),
-    path('create/', views.create, name='click'),
-    path('join/', views.join, name="join"),
-    path('processing/', views.processing, name="processing"),
-    path('profile/', views.profile),
-    path('update-profile/', views.UserUpdatation),
-    path('register/', views.register, name="register"),
+    path('',include('accounts.urls'),    
 ]
